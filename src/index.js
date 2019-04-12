8978047a08d1ba6f01b57b6e38e4ac9db225cb9d
Bailey Burke <bailey.a.burke.20@dartmouth.edu>
2019-04-12 15:37:55 -0400

    new files/code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up!');
+// change require to es6 import style
+import './style.scss';
+import $ from 'jquery';
+
+let seconds = 0;
+
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${seconds} seconds`);
+  seconds += 1;
+}, 1000);
