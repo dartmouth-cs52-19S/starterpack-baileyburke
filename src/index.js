e8279f9766c295c73cbbff23de1dfb02375b1996
Bailey Burke <bailey.a.burke.20@dartmouth.edu>
2019-04-14 15:14:58 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-// change require to es6 import style
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
-import $ from 'jquery';
 
-let seconds = 0;
+const App = () => <div className="test">All the REACT are belong to us!</div>;
 
-setInterval(() => {
-  $('#main').html(`You've been on this page for ${seconds} seconds`);
-  seconds += 1;
-}, 1000);
+ReactDOM.render(<App />, document.getElementById('main'));
+
+
+// change require to es6 import style
+// import './style.scss';
+// import $ from 'jquery';
+
+// let seconds = 0;
+
+// setInterval(() => {
+//   $('#main').html(`You've been on this page for ${seconds} seconds`);
+//   seconds += 1;
+// }, 1000);
